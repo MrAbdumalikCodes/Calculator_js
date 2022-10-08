987228b88b7c9c4d30865c8fbf5defbf25e90855
Abdumalik <abdumalikcodes7753@gmail.com>
2022-10-08 14:52:24 +0500

    if_else_Calculator

---
+++ second_homework/js/script.js
@@ -0,0 +1,13 @@
+let num1 = prompt('Enter your first number !')
+let op  = prompt('Enter opertator!')
+let num2 = prompt('Enter your second number')
+
+if(op =='+'){
+    console.log(+num1 + +num2)
+}else if (op == '-'){
+    console.log(num1-num2)
+}else if (op =='*'){
+    console.log(num1*num2)
+}else if (op == '/'){
+    console.log(num1/num2)
+}
